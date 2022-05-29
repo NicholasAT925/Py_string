ea755aa6f1dd525cdb911d5329aed8b550f36e44
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 18:34:30 +0800

    Update string.py

--- In_py/string.py
+++ In_py/string.py
@@ -11,5 +11,9 @@ s1 = ''
 for c in TheString:
     # s1 = s1 + c # this will produce the same string as given string to reverse
     s1 = c + s1 
+
 print(s1)
-print(TheString[::-1])
+print(TheString[::-1]) # The best and simplest way to revser a string is by slicing
+print("\n")
+for c in TheString[-1::-1]: # printing the letters of the string in reverse
+    print(c)
