80a18844a77c87cc4f0844fd4b77934c3b4154dd
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 18:46:31 +0800

    Update string.py

--- In_py/string.py
+++ In_py/string.py
@@ -1,19 +1,22 @@
 TheString = "Rayla is my Fav Character"
 
 print(TheString)
-print(TheString[3], TheString[4])
+print(TheString[3], TheString[4]) # print the first 2 characters
 print("\n")
-for i in TheString:
+for i in TheString: # print the characters of the string
     print(i)
 print("\n")
-print(TheString[-1], TheString[-2])
-s1 = ''
+print(TheString[-1], TheString[-2]) # printing the last 2 characters of the string
+s1 = '' # emptry variable to hold the new string
 for c in TheString:
     # s1 = s1 + c # this will produce the same string as given string to reverse
     s1 = c + s1 
 
 print(s1)
-print(TheString[::-1]) # The best and simplest way to revser a string is by slicing
+print(TheString[::-1]) # The best and simplest way to reverse a string is by slicing
 print("\n")
 for c in TheString[-1::-1]: # printing the letters of the string in reverse
     print(c)
+print("\n")
+Computer_parts = ["Monitor", "mouse", "PC_Tower"]
+print(Computer_parts[0][2])
